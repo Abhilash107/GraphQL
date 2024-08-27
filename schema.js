4b848ff66aa460ef8c44fe479e589c998d8d016b
Abhilash107 <abhilash00003108884@gmail.com>
2024-08-28 00:16:12 +0530

    writing schema

---
+++ schema.js
@@ -0,0 +1,32 @@
+export const typeDefs = `#graphql
+    type Game {
+        id: ID!
+        title: String!
+        platform: [String!]!
+    }
+
+    type Review {
+        id: ID!
+        rating: Int!
+        content: String!
+    }
+
+    type Author {
+        id: ID!
+        name: String!
+        verified: Boolean!
+    }
+
+    type Query {
+        reviews: [Review]
+        review(id: ID!): Review
+        games: [Game]
+        game(id: ID!): Game
+        authors: [Author]
+        author(id: ID!): Author
+    }
+`
+
+
+// int, float, String, boolean, ID, 
+// use ! to make the field required
